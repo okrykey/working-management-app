81b07f305698aa574cd693e7a47028e5f8a749e0
okrykey <o9mura2000z@gmail.com>
2023-12-10 21:08:20 +0900

    feat: Create basic page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
       <div>Working Management App</div>
+      <Link href="/managements">
+        <p>従業員勤怠管理ページ</p>
+      </Link>
+      <Link href="/admin">
+        <p>管理者ページ</p>
+      </Link>
     </main>
   );
 }
