0a0f8013ff08c239fdc33ebe4198aac4be7f9e56
okrykey <o9mura2000z@gmail.com>
2023-12-12 15:25:06 +0900

    feat: Add and modify design

--- src/app/admin/employee/[id]/page.tsx
+++ src/app/admin/employee/[id]/page.tsx
@@ -119,96 +119,108 @@ const EmployeeDetailPage = async ({ params }: { params: { id: string } }) => {
   const monthlyStats = calculateMonthlyStats(groupedRecords);
 
   return (
-    <div className="flex flex-col items-center justify-center p-6">
-      <h1 className="text-xl font-bold mb-4">{employee?.name}さんの勤怠記録</h1>
-      {monthlyStats.map((stat) => (
-        <div
-          key={stat.month}
-          className="w-full max-w-xl  shadow-sm border border-gray-800 rounded-lg p-4 mb-4"
-        >
-          <h2 className="text-lg font-semibold">{stat.month}月の稼働記録</h2>
-          <p className="text-md">総稼働時間: {stat.totalWorkingTime}</p>
-          <p className="text-md">勤務日数: {stat.totalWorkDays}</p>
-        </div>
-      ))}
+    <main>
+      <div className="flex flex-col items-center justify-center p-6">
+        <h1 className="text-xl font-bold mb-4">
+          {employee?.name}さんの勤怠記録
+        </h1>
 
-      <Table className="text-center">
-        <TableCaption>
-          <Link href={`/admin/employee/${params.id}/edit`}>
-            <Button variant="outline">修正する</Button>
-          </Link>
-        </TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-1/4 text-center">名前</TableHead>
-            <TableHead className="w-1/4 text-center">メールアドレス</TableHead>
-            <TableHead className="w-1/4 text-center">電話番号</TableHead>
-            <TableHead className="w-1/4 text-center">雇用形態</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">{employee.name}</TableCell>
-            <TableCell>{employee.email}</TableCell>
-            <TableCell>{employee.phoneNumber}</TableCell>
-            <TableCell>{employee.position}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-      <div className="w-2/3">
-        <Table className="text-center">
-          <TableCaption>直近の出退勤リスト</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-1/5 text-center">日付</TableHead>
-              <TableHead className="w-1/5 text-center">出勤時間</TableHead>
-              <TableHead className="w-1/5 text-center">退勤時間</TableHead>
-              <TableHead className="w-1/5 text-center">休憩時間</TableHead>
-              <TableHead className="w-1/5 text-center">稼働時間</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {records.map((record: AttendanceRecord) => (
-              <TableRow key={record.id}>
-                <TableCell className="font-medium">
-                  {record.date.toLocaleDateString("ja-JP", {
-                    year: "numeric",
-                    month: "numeric",
-                    day: "numeric",
-                  })}
-                </TableCell>
-                <TableCell>
-                  {record.startTime.toLocaleTimeString("ja-JP", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    hour12: false,
-                  })}
-                </TableCell>
-                <TableCell>
-                  {record.endTime
-                    ? record.endTime.toLocaleTimeString("ja-JP", {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                        hour12: false,
-                      })
-                    : "-"}
-                </TableCell>
-                <TableCell>
-                  {record.breakTime ? `${record.breakTime}分` : "-"}
-                </TableCell>
-                <TableCell>
-                  {calculateWorkingHours(
-                    record.startTime,
-                    record.endTime,
-                    record.breakTime
-                  )}
-                </TableCell>
+        <div className="w-1/2 py-8">
+          <Table className="text-center border rounded-xl">
+            <TableCaption>
+              <Link href={`/admin/employee/${params.id}/edit`}>
+                <Button variant="outline">登録情報を修正する</Button>
+              </Link>
+            </TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-1/4 text-center">名前</TableHead>
+                <TableHead className="w-1/4 text-center">
+                  メールアドレス
+                </TableHead>
+                <TableHead className="w-1/4 text-center">電話番号</TableHead>
+                <TableHead className="w-1/4 text-center">雇用形態</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">{employee.name}</TableCell>
+                <TableCell>{employee.email}</TableCell>
+                <TableCell>{employee.phoneNumber}</TableCell>
+                <TableCell>{employee.position}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          {monthlyStats.map((stat) => (
+            <div
+              key={stat.month}
+              className="w-full max-w-xl shadow-sm border border-gray-800 rounded-lg p-4"
+            >
+              <h2 className="text-lg font-semibold">
+                {stat.month}月の稼働記録
+              </h2>
+              <p className="text-md">総稼働時間: {stat.totalWorkingTime}</p>
+              <p className="text-md">勤務日数: {stat.totalWorkDays}</p>
+            </div>
+          ))}
+        </div>
+        <div className="w-2/3">
+          <Table className="text-center">
+            <TableCaption>直近の出退勤リスト</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-1/5 text-center">日付</TableHead>
+                <TableHead className="w-1/5 text-center">出勤時間</TableHead>
+                <TableHead className="w-1/5 text-center">退勤時間</TableHead>
+                <TableHead className="w-1/5 text-center">休憩時間</TableHead>
+                <TableHead className="w-1/5 text-center">稼働時間</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {records.map((record: AttendanceRecord) => (
+                <TableRow key={record.id}>
+                  <TableCell className="font-medium">
+                    {record.date.toLocaleDateString("ja-JP", {
+                      year: "numeric",
+                      month: "numeric",
+                      day: "numeric",
+                    })}
+                  </TableCell>
+                  <TableCell>
+                    {record.startTime.toLocaleTimeString("ja-JP", {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: false,
+                    })}
+                  </TableCell>
+                  <TableCell>
+                    {record.endTime
+                      ? record.endTime.toLocaleTimeString("ja-JP", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          hour12: false,
+                        })
+                      : "-"}
+                  </TableCell>
+                  <TableCell>
+                    {record.breakTime ? `${record.breakTime}分` : "-"}
+                  </TableCell>
+                  <TableCell>
+                    {calculateWorkingHours(
+                      record.startTime,
+                      record.endTime,
+                      record.breakTime
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
