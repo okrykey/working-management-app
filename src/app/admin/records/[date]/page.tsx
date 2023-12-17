5e4720f9a5969e23e00544d46936e48c8e4d72f3
okrykey <o9mura2000z@gmail.com>
2023-12-17 23:50:26 +0900

    feat: Set falback and set timezone  on  date records table

--- src/app/admin/records/[date]/page.tsx
+++ src/app/admin/records/[date]/page.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from "react";
 import { RecordsTableByDate } from "@/components/recordsTableByDate";
+import { convertUtcToTimeZone } from "@/lib/date";
 
 const RecordByDatePage = ({ params }: { params: { date: string } }) => {
-  const date = new Date(params.date);
+  const date = convertUtcToTimeZone(params.date);
   return (
     <div className="flex min-h-screen flex-col items-center gap-4 p-24">
-      <Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
         <RecordsTableByDate selectedDate={date} />
       </Suspense>
     </div>
