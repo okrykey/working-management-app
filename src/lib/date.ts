572c32772acd289e23e1f5622f3c3eb097c399ae
okrykey <o9mura2000z@gmail.com>
2023-12-17 14:36:14 +0900

    feat: Set time zone by date-fns-tz

---
+++ src/lib/date.ts
@@ -0,0 +1,19 @@
+import { zonedTimeToUtc, utcToZonedTime, format } from "date-fns-tz";
+
+const timeZone = "Asia/Tokyo";
+
+export const convertUtcToTimeZone = (date: Date | string): Date => {
+  return utcToZonedTime(date, timeZone);
+};
+
+export const convertTimeZoneToUtc = (date: Date | string): Date => {
+  return zonedTimeToUtc(date, timeZone);
+};
+
+export const formatInTimeZone = (date: Date, fmt: string): string => {
+  return format(date, fmt, { timeZone });
+};
+
+export const nowInTimeZone = (): Date => {
+  return convertUtcToTimeZone(new Date());
+};
